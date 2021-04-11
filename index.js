565d1b93c9d98e00f0c5b9c9fd15392b367f1d14
RaisaToscano <daisa1690@gmail.com>
2021-04-10 22:20:11 -0500

    Add html, css, js

---
+++ index.js
@@ -0,0 +1,17 @@
+
+    var randomNumber1=Math.floor(Math.random()*6+1);
+    var randomImage1="dice"+randomNumber1+".png"
+        document.querySelector(".img1").setAttribute("src","images/"+randomImage1);
+    var randomNumber2=Math.floor(Math.random()*6+1);
+    var randomImage2="dice"+randomNumber2+".png"
+        document.querySelector(".img2").setAttribute("src","images/"+randomImage2);
+
+        if(randomNumber1>randomNumber2){
+            document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+        }
+        else if(randomNumber1<randomNumber2){
+            document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+        }
+        else {
+            document.querySelector("h1").innerHTML="Draw!";
+        }
